edc207aa2c74fc10eab254a64c23a9b289e0e3be
Max Hrebinets <brainisbro@Gmail.com>
2024-11-12 17:12:57 +0200

    make build

---
+++ index.76449cd8.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall");t.style.display="flex",t.style.borderRadius="40px",t.style.justifyContent="center",t.style.alignItems="center",e.style.position="relative";
+//# sourceMappingURL=index.76449cd8.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const spider = document.querySelector(".spider");
+const wall = document.querySelector(".wall");
+wall.style.display = "flex";
+wall.style.borderRadius = "40px";
+wall.style.justifyContent = "center";
+wall.style.alignItems = "center";
+spider.style.position = "relative";
+
+//# sourceMappingURL=index.f75de5e1.js.map
